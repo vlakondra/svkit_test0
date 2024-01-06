8893d92f7bb1e76a46d3a44216565fbdc78270d4
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-01-06 10:40:16 +0000

    add link to template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<p>
+<a href=" https://github.com/gitpod-samples/template-sveltekit/tree/main">Template
+</a>
+</p>
+
 <h1>Welcome to SvelteKit!!!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
